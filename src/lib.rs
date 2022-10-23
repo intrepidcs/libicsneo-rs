c5eeb4667d8a89eed1f4da78cb0aceb34e5efd8c
David Rebbe <drebbe@intrepidcs.com>
2022-10-23 16:17:12 -0400

    Added get_last_error()

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,53 @@
-use libicsneo_sys::*;
-
-trait SerialNumber {
-    fn get_serial_number(&self) -> String;
-}
-
-impl SerialNumber for neodevice_t {
-    fn get_serial_number(&self) -> String {
-        let serial: String = self.serial.map(|v| v as u8 as char).into_iter().collect();
-        serial
-    }
-}
+//! Rust API for libicsneo.
+//! TODO: Add a better description here
 
 
 pub mod icsneo {
-    use libicsneo_sys::{neodevice_t, icsneo_findAllDevices};
+    use libicsneo_sys::*;
+
+    trait SerialNumber {
+        fn get_serial_number(&self) -> String;
+    }
+    
+    impl SerialNumber for libicsneo_sys::neodevice_t {
+        fn get_serial_number(&self) -> String {
+            let serial: String = self.serial.map(|v| v as u8 as char).into_iter().collect();
+            serial
+        }
+    }
 
     #[derive(Debug)]
     pub enum Error {
         NoDevicesFound,
+        ErrorOccurred(neoevent_t),
     }
 
-    /// TODO
+    /// Calls icsneo_findAllDevices().
+    /// 
+    /// Example:
+    /// ```
+    /// /*
+    /// use libicsneo_rs::*;
+    /// 
+    /// let devices = icsneo::find_all_devices();
+    /// if devices.is_ok() {
+    ///     let devices = devices.unwrap();
+    ///     for device in devices {
+    ///         let serial: String = device.serial.map(|v| v as u8 as char).into_iter().collect();
+    ///         println!("Found device: {serial}");
+    ///     }
+    /// }
+    /// */
+    /// ```
     pub fn find_all_devices() -> Result<Vec<neodevice_t>, Error> {
         // Get the device count
         let device_count = unsafe {
             let mut device_count = 0;
             icsneo_findAllDevices(std::ptr::null_mut(), &mut device_count);
-            // TODO: Check error codes here
+            match get_last_error() {
+                Some(e) => return Err(Error::ErrorOccurred(e)),
+                None => {},
+            };
             device_count
         };
         // We are done if we don't have any devices
@@ -49,14 +70,38 @@ pub mod icsneo {
             if device_count == 0 {
                 return Err(Error::NoDevicesFound);
             }
-            // TODO: Check error codes here
+            match get_last_error() {
+                Some(e) => return Err(Error::ErrorOccurred(e)),
+                None => {},
+            };
         }
         Ok(devices)
+    }
+
+    /// Calls icsneo_getLastError(). Returns the neoevent_t if an error occurred or None if not.
+    pub fn get_last_error() -> Option<neoevent_t> {
+        // TODO: Does description need to be allocated here?
+        let mut neo_event = neoevent_t {
+            description: 0 as *const std::os::raw::c_char,
+            timestamp: 0,
+            eventNumber: 0,
+            severity: 0,
+            serial: [0i8; 7],
+            reserved: [0u8; 16],
+        };
+        unsafe {
+            if icsneo_getLastError(&mut neo_event as *mut _) {
+                Some(neo_event)
+            } else {
+                None
+            }
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
     #[test]
@@ -65,5 +110,10 @@ mod tests {
         let results = icsneo::find_all_devices();
         // Will only be true if no devices are plugged in
         assert_eq!(results.is_err(), true);
+    }
+
+    #[test]
+    fn test_get_last_error() {
+        //assert_matches!(icsneo::get_last_error(), None);
     }
 }
